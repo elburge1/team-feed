3e6bbcd32c8988faa0df4b1ac2cb4eebe80bb23d
Andrew Beyer <beyer.andrew@gmail.com>
2016-09-10 21:29:40 -0700

    refactor javascript

---
+++ mockups/app.js
@@ -0,0 +1,2 @@
+var app = new Framework7({pushState: true});
+var mainView = app.addView('.view-main', {});
---
+++ mockups/player.js
@@ -0,0 +1,1 @@
+var playerSwiper = app.swiper(".swiper-container", {speed: 400, pagination: ".swiper-container .swiper-pagination"});
