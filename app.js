36e92a5e43bb63e8c5f80918be1c8c9b70d50ee3
Edward Burgess <elburgess1@gmail.com>
2016-09-25 15:52:01 -0700

    setting up routes and error handlers for deployment

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
        bodyParser       = require("body-parser"),
        mongoose         = require("mongoose"),
        express          = require("express"),
+       routes           = require('./routes/index'),
        app              = express();
+
 
 // Define location and or methods used by packges
    mongoose.connect("mongodb://localhost/playerdb");
@@ -11,7 +13,31 @@
    app.use(bodyParser.urlencoded({extended: true}));
    app.use(expressSanitizer())
    app.set("view engine", "ejs");
+   app.use('/', routes);
 
-   
+   app.use(function(req, res, next){
+     var err = new Error('Not Found');
+     err.status = 400;
+     next(err);
+   })
+
+   if(app.get('env') === 'development'){
+     app.use(function(req, res, next) {
+       res.status(err.status || 500);
+       res.render('error', {
+         message: err.message,
+         error: err
+       })
+     })
+   }
+
+   app.use(function(err, req, res, next){
+     res.status(err.status || 500);
+     res.render('error', {
+       message: err.message,
+       error: {}
+     });
+   });
+
 
 module.exports = app;
